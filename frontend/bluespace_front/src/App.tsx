d539b3ba8149bd649873efa7b6e5d4b3bdacdcd7
lovekdl <2412162744@qq.com>
2023-07-03 21:51:07 +0800

    feat:前端搭建

---
+++ frontend/bluespace_front/src/App.tsx
@@ -0,0 +1,42 @@
+// Photos from https://citizenofnowhe.re
+import "./App.css";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`;
+const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`;
+
+function Image({ id }: { id: number }) {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isInView, setIsInView] = useState(false);
+
+  return (
+    <section>
+      {'page: ' + id}
+      <motion.div
+        initial={false}
+        animate={
+          isLoaded && isInView
+            ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+            : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+        }
+        transition={{ duration: 1, delay: 1 }}
+        viewport={{ once: true }}
+        onViewportEnter={() => setIsInView(true)}
+      >
+        
+        <img src={`/${id}.jpg`} alt="" onLoad={() => setIsLoaded(true)} />
+      </motion.div>
+    </section>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      {[1, 2].map((image) => (
+        <Image id={image} />
+      ))}
+    </>
+  );
+}
