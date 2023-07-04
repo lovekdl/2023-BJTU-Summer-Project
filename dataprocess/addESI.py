9bcacacdbe7053d7c50ae9adcb1068b6bc655111
zoransy <zoransy@outlook.com>
2023-07-04 14:42:58 +0800

    add PSCompPars

--- dataprocess/addESI.py
+++ dataprocess/addESI.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+import numpy
+import pandas
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+data = pandas.read_excel("../dataset/PSCompPars_2023.07.03_17.48.59.xlsx")
+#print(data)
