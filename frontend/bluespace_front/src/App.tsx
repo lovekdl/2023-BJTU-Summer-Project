a6262ae67c13b2012c9b77bdcc133d2e09bd1cee
lovekdl <2412162744@qq.com>
2023-07-06 10:20:03 +0800

    fix: delete register router

--- frontend/bluespace_front/src/App.tsx
+++ frontend/bluespace_front/src/App.tsx
@@ -28,7 +28,6 @@ export default function App() {
         />
 
       <Route path = "/login" element={<LoginForm />} />
-      <Route path = "/register" element={<RegisterForm/>}></Route>
     </Routes>
 
       
