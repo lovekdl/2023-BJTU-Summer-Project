fb6cd8e53af66e45bd29af1f35000c000a9ab93a
lovekdl <2412162744@qq.com>
2023-07-07 08:46:01 +0800

    feat:prediction page

--- frontend/bluespace_front/src/forecast/prediction.tsx
+++ frontend/bluespace_front/src/forecast/prediction.tsx
@@ -1,40 +1,86 @@
-import React from 'react';
-import "./forecast.style.css"
+import React,{useRef,useState,useEffect} from 'react';
 import type { MenuProps } from "antd";
 import { Breadcrumb, Layout, Menu, theme } from "antd";
 import { UploadOutlined, VideoCameraOutlined } from '@ant-design/icons';
-
-
-const { Header, Content, Sider } = Layout;
-
-const items1: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
-  key,
-  label: `nav ${key}`
-}));
-
+import "./forecast.style.css"
+import {motion} from "framer-motion";
+import { observer } from 'mobx-react-lite'
+import {Avatar} from 'antd'
+import { useStore } from '../store';
 import {
-  LaptopOutlined,
-  NotificationOutlined,
   UserOutlined
 } from "@ant-design/icons";
 
-export default function Prediction() {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+
+
+const { Content, Sider } = Layout;
+function Prediction() {
+  
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [nowKey, setNowKey] = useState(1);
+  const [content, setContent] = useState(<div></div>);
+
+  const {ProfileStore} = useStore();
+  function getLabel(x:number) {
+    if(x === 1) {
+      return "Statistics"
+    }
+    if(x === 2) {
+      return "Analysis"
+    }
+    if(x === 3) {
+      return "Prediction"
+    }
+  }
+  useEffect(()=>{
+    if(nowKey == 1) {//Statistics
+      setContent(
+      <div>
+        
+      </div>
+      )
+    } 
+    if(nowKey == 2) {//Analysis
+      setContent(
+      <div>
+        
+      </div>
+      )
+    } 
+    if(nowKey == 3) {//Prediction
+      setContent(
+      <div>
+        
+      </div>
+      )
+    } 
+  },[nowKey])
+  const handleUploadClicked = ()=> {
+    console.log('hahaha')
+    if(fileInputRef.current)
+    fileInputRef.current.click();
+  }
+  const handleFileChange = (event:any) => {
+    const imgfile = event.target.files[0];
+    console.log(imgfile);
+    var reader = new FileReader();
+    reader.onload=function(){
+      var fileurl = reader.result
+      ProfileStore.setAvatar(fileurl)
+      console.log('fileurl is ' + fileurl);
+    }
+    reader.readAsDataURL(imgfile)
+
+  };
+  
+  function handleLeftMenuClicked  ({key} : any) {
+    console.log(key)
+    setNowKey(key)
+  }
   return (
     <div>
+      
       <Layout>
-        {/* <Sider width={300} style={{ background: colorBgContainer }}>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-            className = 'SliderMenu'
-            
-            items={items2}  
-          />
-        </Sider> */}
         <Sider
         breakpoint="lg"
         collapsedWidth="0"
@@ -49,33 +95,31 @@ export default function Prediction() {
         <div className="demo-logo-vertical" />
         <Menu
           mode="inline"
-          defaultSelectedKeys={['4']}
+          defaultSelectedKeys={['1']}
           className='SliderMenu'
-          items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+          items={[UserOutlined, VideoCameraOutlined, UploadOutlined].map(
             (icon, index) => ({
               key: String(index + 1),
               icon: React.createElement(icon),
-              label: `nav ${index + 1}`,
+              label: <span >{`${getLabel(index+1)}`}</span>,
             }),
           )}
+          onSelect={handleLeftMenuClicked}
         />
       </Sider>
 
       
         <Layout style={{ padding: "0 24px 0px" }}>
+          
+          <Content className='ProfileContent'>
             
-          <Content
-            style={{
-              padding: 24,
-              margin: 5,
-              minHeight: 100,
-              background: colorBgContainer
-            }}
-          >
-            
+            {content}
+
           </Content>
         </Layout>
       </Layout>
     </div>
   )
 }
+
+export default observer(Prediction);
--- frontend/bluespace_front/src/userprofile/profile.tsx
+++ frontend/bluespace_front/src/userprofile/profile.tsx
@@ -1,4 +1,4 @@
-import React,{useRef} from 'react';
+import React,{useRef,useState,useEffect} from 'react';
 import type { MenuProps } from "antd";
 import { Breadcrumb, Layout, Menu, theme } from "antd";
 import { UploadOutlined, VideoCameraOutlined } from '@ant-design/icons';
@@ -17,6 +17,8 @@ const { Content, Sider } = Layout;
 function Profile() {
   
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [nowKey, setNowKey] = useState(1);
+  const [content, setContent] = useState(<div></div>);
 
   const {ProfileStore} = useStore();
   function getLabel(x:number) {
@@ -30,7 +32,40 @@ function Profile() {
       return "planets"
     }
   }
-
+  useEffect(()=>{
+    if(nowKey == 1) {
+      setContent(
+      <div>
+        <div className = 'HeadPicture'>
+          {ProfileStore.avatar===''? <Avatar size={128} icon={<UserOutlined />} /> : <Avatar size={128} src={<img src={ProfileStore.avatar}  alt="avatar" />} />}
+          <input
+            type="file"
+            accept="image/*"
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+            onChange={handleFileChange}
+          />
+          <motion.button
+            className="ProfileBox" 
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{ type: "spring", stiffness: 400, damping: 20 }}
+            onClick={handleUploadClicked}
+          >
+            Upload
+          </motion.button>
+        </div>
+        <div className = 'InnerDiv'>
+          <span className = 'ProfileSpan'>Email Address</span>
+          <input type="text" readOnly placeholder="Enter your email"></input>
+        </div>
+      </div>
+      )
+    } 
+    else {
+      setContent(<div></div>);
+    } 
+  },[nowKey])
   const handleUploadClicked = ()=> {
     console.log('hahaha')
     if(fileInputRef.current)
@@ -42,14 +77,20 @@ function Profile() {
     var reader = new FileReader();
     reader.onload=function(){
       var fileurl = reader.result
-      ProfileStore.setAvatar(fileurl) // 这个 useState 把获取的base64 给到img 的src 上
+      ProfileStore.setAvatar(fileurl)
       console.log('fileurl is ' + fileurl);
     }
     reader.readAsDataURL(imgfile)
 
   };
+  
+  function handleLeftMenuClicked  ({key} : any) {
+    console.log(key)
+    setNowKey(key)
+  }
   return (
     <div>
+      
       <Layout>
         <Sider
         breakpoint="lg"
@@ -71,9 +112,10 @@ function Profile() {
             (icon, index) => ({
               key: String(index + 1),
               icon: React.createElement(icon),
-              label: `${getLabel(index+1)}`,
+              label: <span >{`${getLabel(index+1)}`}</span>,
             }),
           )}
+          onSelect={handleLeftMenuClicked}
         />
       </Sider>
 
@@ -81,29 +123,8 @@ function Profile() {
         <Layout style={{ padding: "0 24px 0px" }}>
           
           <Content className='ProfileContent'>
-            <div className = 'HeadPicture'>
-              {ProfileStore.avatar===''? <Avatar size={128} icon={<UserOutlined />} /> : <Avatar size={128} src={<img src={ProfileStore.avatar}  alt="avatar" />} />}
-              <input
-                type="file"
-                accept="image/*"
-                ref={fileInputRef}
-                style={{ display: 'none' }}
-                onChange={handleFileChange}
-              />
-              <motion.button
-                className="ProfileBox" 
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                transition={{ type: "spring", stiffness: 400, damping: 20 }}
-                onClick={handleUploadClicked}
-              >
-                Upload
-              </motion.button>
-
-
-              
-            </div>
-
+            
+            {content}
 
           </Content>
         </Layout>
